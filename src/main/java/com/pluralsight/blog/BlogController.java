7398d4b6dc076b963cd7bf82d3b166ada112a970
Mohit Panati Venkat <mohit.panativenkat@boeing.com>
2021-06-25 13:33:26 +0530

    Commit 4 Post details

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -4,6 +4,7 @@ import com.pluralsight.blog.data.PostRepository;
 import com.pluralsight.blog.model.Post;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -31,6 +32,13 @@ public class BlogController {
         modelMap.put("title","Blog Post 1");
         modelMap.put("posts",postList);
         return "home";
+    }
+
+    @RequestMapping("/post/{id}")
+    public String postDetails(@PathVariable Long id, ModelMap modelMap ){
+        Post post = postRepository.findById(id);
+        modelMap.put("post",post);
+        return "post-details";
     }
 
 }
